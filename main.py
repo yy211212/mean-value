117b380bfc67712ff5f8148c1ec935307e893d9d
yy211212 <limyuyoung211212@gmail.com>
2024-11-29 17:12:49 +0800

    commit

---
+++ main.py
@@ -0,0 +1,10 @@
+mean1 = 38
+wrong_number=36
+correct_number=56
+total_number=40
+sum = mean1*total_number
+print("the sum of 40  number: ", sum)
+num2=sum-((wrong_number)-(correct_number))
+print("sum-((wrong_number)-(correct_number)):",num2)
+mean2=num2/total_number
+print(mean2)
